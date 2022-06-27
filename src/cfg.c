ee07a7c61178408d21d247ca3762e775ea5d41b5
Saifeddine <aloui.seifeddine@gmail.com>
2022-06-27 22:55:23 +0200

    removed unnecessary import

--- src/cfg.c
+++ src/cfg.c
@@ -20,7 +20,6 @@
 
 #include <fcntl.h>      // Contains file controls like O_RDWR
 #include <errno.h>      // Error integer and strerror() function
-#include <termios.h>    // Contains POSIX terminal control definitions
 #include <unistd.h>     // write(), read(), close()
 
 
